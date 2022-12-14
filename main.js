625b7880cd32eaf5dbcdc996eec116588ec51117
PrernaBajajj <prerna0549.be20@chitkara.edu.in>
2022-12-14 12:50:19 +0530

    Game done

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
-console.log("Hello")
+console.log("Welcome to Tic Tac Toe")
+
+let turn = "X"
+let isgameover = false;
+
 // Function to change the turn
 const changeTurn = ()=>{
     return turn === "X"? "0": "X"
 }
+
+// Function to check for a win
 const checkWin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e =>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
+            isgameover = true
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            
+        }
+    })
 }
+
+// Game Logic
+
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element =>{
     let boxtext = element.querySelector('.boxtext');
@@ -14,9 +42,21 @@ Array.from(boxes).forEach(element =>{
             turn = changeTurn();
             
             checkWin();
-            
+            if (!isgameover){
                 document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
-            
+            } 
         }
     })
+})
+//ssAdd onclick listener to reset button
+reset.addEventListener('click', ()=>{
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X"; 
+    isgameover = false
+    document.querySelector(".line").style.width = "0vw";
+    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
 })
